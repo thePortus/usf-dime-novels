586ae42489aa52c9ae81f034d470de5d8724746b
David J. Thomas <dave.a.base@gmail.com>
2017-06-26 11:31:59 -0400

    firefox and chrome installations added 9

--- usf_dime_novels/db/__init__.py
+++ usf_dime_novels/db/__init__.py
@@ -16,23 +16,38 @@ from usf_dime_novels.common import settings, Printer
 
 
 def database(mode='live'):
-    sql.SqliteDatabase(settings.LIVE_DB)
+    """
+    Returns the sqlite database object depending on live or testing mode.
+    Currently redundant, since live & testing databases are the same.
+    """
+    # Return testing dB is specified, otherwise, live dB
+    if mode == 'testing':
+        return sql.SqliteDatabase(settings.TESTING_DB)
+    return sql.SqliteDatabase(settings.LIVE_DB)
 
 
 def init_db(mode='live'):
+    """
+    Performs all the initial operations to check the status of the database.
+    If tables do not exist for schema models, tables will be created.
+    """
+    # List of dB models from the schema
     table_list = [
         # List each table model here
     ]
     db = database(mode=mode)
     db.connect()
     missing_tables = []
+    # Check each model for a table, append missing to missing_tables
     for table in table_list:
         if not table.table_exists():
             missing_tables.append(table)
+    # If missing tables are found, create them
     if len(missing_tables):
         Printer('Building database...')
         for table in missing_tables:
             table.create_table()
         Printer('Database built')
+    # Close tb connection
     db.close()
     return True
